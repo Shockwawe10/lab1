f14d6cea7de9260684b315db08545ec4f0fb2ced
Shockwawe10 <vitalik.vuziy@gmail.com>
2023-04-19 20:59:03 +0300

    Change label

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$( document ).ready(function() {
+  new WOW().init();
+});
+b.onclick=function(){
+ Swal.fire({
+  position: 'center',
+  icon: 'success',
+  title: 'Thank you!',
+  showConfirmButton: false
+})
+    return false;
+}
